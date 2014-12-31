1f1926f78f678c5ad59bd282512078f59105e1f3
Tom Lyons <tbl3rd@gmail.com>
2014-12-31 12:59:39 -0500

    This is probably how I'd do it.

--- src/elevator/core.clj
+++ src/elevator/core.clj
@@ -1,29 +1,60 @@
 (ns elevator.core)
 
-(def top 10)
-(def bottom 1)
+(def elevator
+  "An elevator for illustration."
+  {:floor 1 :direction :up})
 
-(def down 0)
+(def direction?
+  "Nil or the function to apply to :floor for direction :down or :up."
+  {:down dec :up inc})
 
-(def direction? #{:down :up})
+(def bottom
+  "The elevator's bottom floor."
+  1)
 
-(def elevator {:floor 1 :direction :up})
+(def top
+  "The elevator's top floor."
+  10)
 
 (defn move
+  "Move an elevator subject to TOP and BOTTOM."
   [{:keys [floor direction]}]
   {:pre [(integer? floor) (direction? direction)]}
-  (let [floor ((direction {:up inc :down dec}) floor)]
+  (let [floor ((direction? direction) floor)]
     (cond (> floor top)    {:floor top    :direction :down}
           (< floor bottom) {:floor bottom :direction :up}
           :else            {:floor floor  :direction direction})))
 
 (move elevator)
-{:floor 2, :direction :up}
 
-(move {:floor 9, :direction :up})
-(move {:floor 10, :direction :up})
-(move {:floor 1, :direction :down})
-{:floor 1, :direction :up}
-{:floor 10, :direction :down}
+(comment
 
-(move {:floor 7 :direction :fnord})
+  (clojure.pprint/pprint (take 23 (iterate move elevator)))
+
+  "... prints ..."
+
+  ({:floor  1, :direction :up}
+   {:floor  2, :direction :up}
+   {:floor  3, :direction :up}
+   {:floor  4, :direction :up}
+   {:floor  5, :direction :up}
+   {:floor  6, :direction :up}
+   {:floor  7, :direction :up}
+   {:floor  8, :direction :up}
+   {:floor  9, :direction :up}
+   {:floor 10, :direction :up}
+   {:floor 10, :direction :down}
+   {:floor  9, :direction :down}
+   {:floor  8, :direction :down}
+   {:floor  7, :direction :down}
+   {:floor  6, :direction :down}
+   {:floor  5, :direction :down}
+   {:floor  4, :direction :down}
+   {:floor  3, :direction :down}
+   {:floor  2, :direction :down}
+   {:floor  1, :direction :down}
+   {:floor  1, :direction :up}
+   {:floor  2, :direction :up}
+   {:floor  3, :direction :up})
+
+  "... in the REPL.")
